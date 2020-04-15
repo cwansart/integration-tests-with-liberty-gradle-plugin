004489f23c0ec9629607d8ed6e293617c74cf563
Christian Wansart <christian.wansart@openknowledge.de>
2020-04-15 12:41:16 +0200

    Add output for testing

--- src/test/java/gradle/liberty/plugin/integration/tests/HelloResourceIT.java
+++ src/test/java/gradle/liberty/plugin/integration/tests/HelloResourceIT.java
@@ -2,15 +2,22 @@ package gradle.liberty.plugin.integration.tests;
 
 import io.restassured.RestAssured;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.hamcrest.CoreMatchers.is;
 
 public class HelloResourceIT {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HelloResourceIT.class);
+
     public static final String URL = "http://localhost:9080/gradle-liberty-plugin-for-integration-tests/api/hello";
 
     @Test
     public void getHello() {
+
+        LOG.info("Calling HelloResourceIT.getHello() test.");
+
         RestAssured.when()
             .get(URL)
             .then()
